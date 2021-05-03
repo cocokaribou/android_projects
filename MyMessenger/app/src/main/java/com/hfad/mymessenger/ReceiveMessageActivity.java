bf6828e032b336d92351796a3439d96747936b87
Your Name <you@your-domain.com>
2021-05-03 22:31:38 +0900

    init commit

---
+++ MyMessenger/app/src/main/java/com/hfad/mymessenger/ReceiveMessageActivity.java
@@ -0,0 +1,24 @@
+package com.hfad.mymessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+
+        Intent intent = getIntent(); //액티비티를 시작시킨 인텐트 반환. 인텐트에 포함된 추가정보 추출 가능
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView) findViewById(R.id.message);
+        messageView.setText(messageText);
+
+    }
+}
