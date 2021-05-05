55dd65f9b1fe68aed78aefb495299818086633de
Your Name <you@your-domain.com>
2021-05-05 18:08:14 +0900

    edit MyMessenger project

--- MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -18,8 +18,15 @@ public class CreateMessageActivity extends AppCompatActivity {
     public void onSendMessage(View view){
         EditText messageView = (EditText) findViewById(R.id.message);
         String messageText = messageView.getText().toString();
-        Intent intent = new Intent(this, ReceiveMessageActivity.class);
-        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+
+//        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+//        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+
+        Intent intent = new Intent(Intent.ACTION_SEND); //안드로이드가 제공하는 표준 액션
+        //명시적으로 ReceiveMesaageActivity를 요청하는 인텐트 대신 전송액션 사용하는 인텐트 생성
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+
         startActivity(intent);
     }
 }
