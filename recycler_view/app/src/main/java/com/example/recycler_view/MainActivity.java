297da5cd97370495164e5586aec37f0c8563a48a
yilee_pionnnet <joyfuljuli@pionnet.co.kr>
2021-05-07 09:26:40 +0900

    editted MainActivity.java file

--- recycler_view/app/src/main/java/com/example/recycler_view/MainActivity.java
+++ recycler_view/app/src/main/java/com/example/recycler_view/MainActivity.java
@@ -47,81 +47,51 @@ public class MainActivity extends AppCompatActivity {
                 mLinearLayoutManager.getOrientation());
         mRecyclerView.addItemDecoration(dividerItemDecoration);
 
-//        mRecyclerView.addOnItemTouchListener(new RecycleTouchListener(getApplicationContext(), mRecyclerView, new ClickListener(){
-//
-//        }));
 
         //insert 버튼에 액티비티 달아주기
-        Button buttonInsert = (Button)findViewById(R.id.button_main_insert);
-        Button buttonDelete = (Button)findViewById(R.id.button_main_delete);
-        buttonInsert.setOnClickListener(new View.OnClickListener(){
+        Button buttonInsert = (Button) findViewById(R.id.button_main_insert);
+        Button buttonDelete = (Button) findViewById(R.id.button_main_delete);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 count++;
 
-                Dictionary data = new Dictionary(count+"", "Apple"+count, "사과"+count);
+                Dictionary data = new Dictionary(count + "", "Apple" + count, "사과" + count);
                 //mArrayList.add(0, data); //recyclerview 의 첫줄에 삽입
                 mArrayList.add(data); // recyclerview의 마지막줄에 삽입
 
                 //mAdapter.notifyDataSetChanged(); //화면이 다 바뀐다
                 //mAdapter.notifyItemChanged(1); //특정 뷰홀더만 바꾼다 이거 희한하게 동작하네!
-                //mAdapter.notifyItemInserted(count); //특정 뷰홀더 위치에 삽입한다. 이게 함수를 덜 불러내는데
-                mAdapter.notifyItemMoved(count, 0); //
+                //mAdapter.notifyItemInserted(count); //특정 뷰홀더 위치에 삽입한다. 이게 함수를 덜 불러내는데(성능상 좋겠지 당연히?)
+                //mAdapter.notifyItemMoved(0, count); //negative delta라는 에러가 뜨면서 앱이 강제종료된다
+                //mAdapter.notifyItemMoved(0, count-1);
+                mAdapter.notifyItemRangeChanged(0, count); //itemChanged랑 하는 짓 생김새는 비슷함
 
 
-                if(count>0){
+                if (count > 0) {
                     buttonDelete.setVisibility(View.VISIBLE);
-                }else if(count<1){
+                } else if (count < 1) {
                     buttonDelete.setVisibility(View.GONE);
                 }
             }
         });
 
         //리스트 삭제 버튼도 만들어보자.....
-        buttonDelete.setOnClickListener(new View.OnClickListener(){
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 count--;
 
-                if(mArrayList.size()>0){
-                    mArrayList.remove(mArrayList.size()-1);
+                if (mArrayList.size() > 0) {
+                    mArrayList.remove(mArrayList.size() - 1);
                 }
-                //아래 조건을 else if로 주니 항상 true로 나왔는데...
-                if(mArrayList.size()<=0){
+                if (mArrayList.size() <= 0) {
                     buttonDelete.setVisibility(View.GONE);
                     count = 0;
                 }
-                mAdapter.notifyDataSetChanged();
+                //mAdapter.notifyDataSetChanged(); //전체 갱신 하면서 bind create 다시 호출
+                mAdapter.notifyItemChanged(count); //삭제된 데이터를 지우면서 bind create 호출 x
             }
         });
-
-
     }
-
-    public interface ClickListener{
-        void onClick(View view, int position);
-        void onLongClick(View view, int position);
-    }
-
-//    public static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener{
-//        private GestureDetector gestureDetector;
-//        private MainActivity.ClickListener clickListener;
-//
-//        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final MainActivity.ClickListener clickListener){
-//            this.clickListener = clickListener;
-//            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener(){
-//                @Override
-//                public boolean onSingleTapUp(MotionEvent e){
-//                    return true;
-//                }
-//
-//                @Override
-//                public void onLongPress(MotionEvent e){
-//                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
-//
-//                }
-//            })
-//        }
-//    }
-
 }
