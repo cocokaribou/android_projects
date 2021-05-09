dfa9d0e652b29647a8478ece85553f08b6271a46
Your Name <you@your-domain.com>
2021-05-09 23:48:15 +0900

    modify StopwatchActivity.java file

--- Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -3,12 +3,60 @@ package com.hfad.stopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class StopwatchActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
+    }
+
+    //start 버튼을 클릭하면 스톱워치 시작
+    public void onClickStart(View view){
+        running = true;
+    }
+
+    //stop 버튼을 클릭하면 스톱워치 중지
+    public void onClickStop(View view){
+        running = false;
+    }
+
+    //reset 버튼을 클릭하면 스톱워치 리셋
+    public void onClickReset(View view){
+        running = false;
+        seconds = 0;
+    }
+
+    //runTimer()
+    private void runTimer(){
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+
     }
 }
