e55775a62b61e03ad4d236fd3cfd93c10914ad1e
cocokaribou <guiltycoco@gmail.com>
2021-05-12 11:52:24 +0900

    modify StopWatchActivity.java file; add methods

--- Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -18,60 +18,84 @@ public class StopwatchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("⚠️", "onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
-        if(savedInstanceState != null){
-            seconds = savedInstanceState.getInt("seconds"); //bundle에서 값을 얻어서 액티비티의 상태를 복원
-            running = savedInstanceState.getBoolean("running");
-            wasRunning = savedInstanceState.getBoolean("wasRunning");
-        }
+//        if(savedInstanceState != null){
+//            seconds = savedInstanceState.getInt("seconds"); //bundle에서 값을 얻어서 액티비티의 상태를 복원
+//            running = savedInstanceState.getBoolean("running");
+//            wasRunning = savedInstanceState.getBoolean("wasRunning");
+//        }
         runTimer();
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
+        Log.d("⚠️", "onSaveInstanceState()");
+
         super.onSaveInstanceState(savedInstanceState); //이거 책에는 없는 부분인데 super를 안 부르면 실행이 안되나..?
+        //Activity.java에는 @Callsuper 어노테이션이 없는데...
+
+        //bundle은 key-value 쌍의 데이터를 모아놓는 곳
+        //java의 map과 비슷하지만 안드로이드에 필요한 기능이 좀 더 많음
+        //put자료형()
         savedInstanceState.putInt("seconds", seconds);
         savedInstanceState.putBoolean("running", running);
         savedInstanceState.putBoolean("wasRunning", wasRunning);
     }
 
     @Override
-    protected void onStop(){
+    protected void onStop() {
+        Log.d("⚠️", "onStop()");
+
         super.onStop();
         wasRunning = running; //onStop()가 호출됐을 때 스톱워치가 실행중이었는지
         running = false;
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
+        Log.d("⚠️", "onStart()");
+
         super.onStart();
-        if(wasRunning){
+        if (wasRunning) {
             running = true;
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        Log.d("⚠️", "onDestroy()");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("⚠️", "onResume()");
+        super.onResume();
+    }
+
 
     //start 버튼을 클릭하면 스톱워치 시작
-    public void onClickStart(View view){
+    public void onClickStart(View view) {
         running = true;
     }
 
     //stop 버튼을 클릭하면 스톱워치 중지
-    public void onClickStop(View view){
+    public void onClickStop(View view) {
         running = false;
     }
 
     //reset 버튼을 클릭하면 스톱워치 리셋
-    public void onClickReset(View view){
+    public void onClickReset(View view) {
         running = false;
         seconds = 0;
     }
 
     //왜 버튼 onclick 메서드들은 public인데 runTimer는 private이지..?
     //runTimer()
-    private void runTimer(){
-        final TextView timeView = (TextView)findViewById(R.id.time_view);
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
 
         //Handler 클래스의 객체 생성
         final Handler handler = new Handler();
