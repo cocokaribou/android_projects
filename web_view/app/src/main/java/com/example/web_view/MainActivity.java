7c3c03e872e33dfe5b21ae58f21d456b52db20aa
cocokaribou <younginlee2017@gmail.com>
2021-05-12 15:53:06 +0900

    add assets

--- web_view/app/src/main/java/com/example/web_view/MainActivity.java
+++ web_view/app/src/main/java/com/example/web_view/MainActivity.java
@@ -12,7 +12,8 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
-    private String url = "https://www.naver.com";
+    //private String url = "https://www.naver.com";
+    private String url = "file:///android_asset/www/index.html"; //<script> alert("test"); </script>
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +29,21 @@ public class MainActivity extends AppCompatActivity {
 
         //webView 객체를 요소로 가져오거나 아니면 뷰그룹의 context로 가져오거나
 
-        //setContentView(myWebView);
-        //setContentView(R.layout.activity_main); //위의 코드와 똑같이 크롬으로 리다이렉트
-
-        //아냐 역시 setContentView는 메인을 불러오는 게 맞다 상식적으로도
-
         webView.getSettings().setJavaScriptEnabled(true); //이거 안 붙이면 사이트 다 망가지나? -> 똑같음
         webView.loadUrl(url);
 
 
+        webView.setWebViewClient(new WebViewClientClass());
         webView.setWebChromeClient(new WebChromeClient());
-        webView.setWebViewClient(new WebViewClient());
-    }
 
+        //webview로 구성된 화면에서 js로 alert 메시지를 띄울때, WebViewClient로만 구성하면 메시지가 뜨지않는다
+
+    }
     //웹페이지 뒤로가기 지원하는 함수
     //없으면 뒤로가기 버튼 눌렀을때 앱이 꺼진다
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event){
+        //뒤로가기 버튼이 눌렸음 && 뒤로갈 페이지가 존재할때
         if((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()){
             webView.goBack();
             return true;
@@ -58,6 +57,12 @@ public class MainActivity extends AppCompatActivity {
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
+        }
+
+        //webView로 구성된 화면에서 js로 alert띄울때, webViewClient로만 구성하면 alert창이 안 뜸
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            super.onReceivedError(view, errorCode, description, failingUrl);
         }
     }
 }
