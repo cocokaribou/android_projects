958c5fd4f6408b417c89f4d85d0a0614b2412642
cocokaribou <younginlee2017@gmail.com>
2021-05-24 08:58:56 +0900

    [Update] swipe_view dialog뜨게 수정

--- swipe_view/app/src/main/java/com/hfad/swipe_view/MainActivity.java
+++ swipe_view/app/src/main/java/com/hfad/swipe_view/MainActivity.java
@@ -1,9 +1,11 @@
 package com.hfad.swipe_view;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +18,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         viewPager = (ViewPager) findViewById(R.id.viewPager);
+        View view = findViewById(R.id.click_view);
         myPagerAdapter = new MyPagerAdapter(this);
         viewPager.setAdapter(myPagerAdapter);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == myPagerAdapter.getCount() - 1) {
+                    view.setVisibility(View.VISIBLE);
+                } else {
+                    view.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setView(R.layout.activity_dialog);
+
+                AlertDialog alertDialog = builder.create();
+
+                alertDialog.show();
+            }
+        });
     }
 }
