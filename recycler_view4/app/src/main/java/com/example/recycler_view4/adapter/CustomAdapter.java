e7cf71b65a1d7ecdc160d2ef900840cbe5b9cce8
cocokaribou <younginlee2017@gmail.com>
2021-06-06 23:48:36 +0900

    [Update] recycler_view4 expandable viewHolder 작업중

--- recycler_view4/app/src/main/java/com/example/recycler_view4/adapter/CustomAdapter.java
+++ recycler_view4/app/src/main/java/com/example/recycler_view4/adapter/CustomAdapter.java
@@ -1,5 +1,7 @@
 package com.example.recycler_view4.adapter;
 
+import android.content.Context;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.recycler_view4.R;
 import com.example.recycler_view4.data.Goods;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -24,17 +27,26 @@ public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         this.goodsList = goodsList;
     }
 
+    private Context context;
+
+    //item의 클릭상태를 저장할 array 객체
+    private SparseBooleanArray selectedItems = new SparseBooleanArray();
+
+    //직전에 클릭됐던 item의 position
+    private int prePosition = -1;
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
         switch (viewType) {
             case 1: {
-                View view = LayoutInflater.from(parent.getContext())
+                View view = LayoutInflater.from(context)
                         .inflate(R.layout.item_surprise, parent, false);
                 return new surpriseHolder(view);
             }
             default: {
-                View view = LayoutInflater.from(parent.getContext())
+                View view = LayoutInflater.from(context)
                         .inflate(R.layout.item_list, parent, false);
                 return new listHolder(view);
             }
@@ -43,14 +55,10 @@ public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+
         if (holder instanceof listHolder) {
-            int index = position;
-            if (RAND == 0 || position > RAND) {
-                index--;
-            }
-            ((listHolder) holder).name.setText(goodsList.get(index).getName());
-            ((listHolder) holder).desc.setText(goodsList.get(index).getDesc());
-            ((listHolder) holder).img.setImageResource(goodsList.get(index).getImg());
+            ((listHolder) holder).onBind(goodsList, position);
         }
     }
 
@@ -68,10 +76,13 @@ public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         return goodsList.size() + 1;
     }
 
-    class listHolder extends RecyclerView.ViewHolder {
+    class listHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView name;
         ImageView img;
         TextView desc;
+
+        private ArrayList<Goods> list;
+        private int position;
 
         public listHolder(@NonNull View itemView) {
             super(itemView);
@@ -80,11 +91,61 @@ public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
             img = itemView.findViewById(R.id.img_itemImg);
             desc = itemView.findViewById(R.id.txt_itemDesc);
         }
+
+        void onBind(ArrayList<Goods> list, int position) {
+            this.list = list;
+            this.position = position;
+            int index = position;
+            if (RAND == 0 || position > RAND) {
+                index--;
+            }
+            name.setText(goodsList.get(index).getName());
+            desc.setText(goodsList.get(index).getDesc());
+            img.setImageResource(goodsList.get(index).getImg());
+
+            changeVisibility(selectedItems.get(position));
+
+            name.setOnClickListener(this);
+            desc.setOnClickListener(this);
+            img.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.txt_itemNm:
+                case R.id.txt_itemDesc:
+                case R.id.img_itemImg: {
+                    if (selectedItems.get(position)) {
+                        selectedItems.delete(position);
+                    } else {
+                        selectedItems.delete(prePosition);
+                        selectedItems.put(position, true);
+                    }
+
+                    if (prePosition != -1) notifyItemChanged(prePosition);
+                    notifyItemChanged(position);
+                    prePosition = position;
+                    break;
+                }
+                default: {
+
+                }
+            }
+        }
     }
 
     class surpriseHolder extends RecyclerView.ViewHolder {
         public surpriseHolder(@NonNull View itemView) {
             super(itemView);
         }
+    }
+
+    private void changeVisibility(final boolean isExpanded){
+        int dpValue = 150;
+        float d = context.getResources().getDisplayMetrics().density;
+        int height = (int)(dpValue * d);
+
+
     }
 }
