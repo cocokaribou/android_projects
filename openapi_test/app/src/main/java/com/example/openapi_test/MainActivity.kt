58ed2891e5961aa3b80eb0d9ed66cf399c2091e5
cocokaribou <younginlee2017@gmail.com>
2021-06-12 18:40:50 +0900

    [Update] api에서 값 읽어오기 작업중
    android.os.NetworkOnMainThreadException 예외

--- openapi_test/app/src/main/java/com/example/openapi_test/MainActivity.kt
+++ openapi_test/app/src/main/java/com/example/openapi_test/MainActivity.kt
@@ -2,20 +2,29 @@ package com.example.openapi_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.NetworkOnMainThreadException
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.openapi_test.databinding.ActivityMainBinding
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.*
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
 
-    private val KEY = "524d4b724e6a6f79313037777a6e6377"
-    private val domain = "http://openapi.seoul.go.kr:8088/(인증키)/xml/LOCALDATA_072218_GS/1/5/"
-    private val pattern = "\\([^)]*\\)" //괄호안 정규식
+    private val KEY: String = "demo" //TODO 하드코딩된 api key
+    private var domain: CharSequence =
+        "http://openapi.seoul.go.kr:8088/(인증키)/json/LOCALDATA_072218_GS/1/5/"
+    private val pattern: Regex = "\\([^)]*\\)".toRegex() //괄호안 정규식
 
     lateinit var binding: ActivityMainBinding
-    lateinit var mainRecycler : RecyclerView
-    lateinit var mainAdapter : mAdapter
-    lateinit var linearManager : LinearLayoutManager
+    lateinit var mainRecycler: RecyclerView
+    lateinit var mainAdapter: mAdapter
+    lateinit var linearManager: LinearLayoutManager
 
     private val list = arrayListOf<String>()
 
@@ -28,17 +37,50 @@ class MainActivity : AppCompatActivity() {
         requestApi()
     }
 
-    fun setRecycler(){
+    fun setRecycler() {
         mainAdapter = mAdapter(list)
         linearManager = LinearLayoutManager(this@MainActivity)
-        binding.recyclerViewMainList.also{
+        binding.recyclerViewMainList.also {
             it.adapter = mainAdapter
             it.layoutManager = linearManager
         }
     }
 
-    fun requestApi(){
-        
+    fun requestApi() {
+        domain = domain.replace(pattern, KEY)
+        try {
+            val url = URL(domain as String)
+
+            val stream: InputStream = url.openStream()
+            val streamReader = InputStreamReader(stream)
+            val reader = BufferedReader(streamReader)
+
+            val buffer = StringBuffer()
+            var line = reader.readLine()
+
+            while(!line.isNullOrBlank()){
+                buffer.append(line + "\n")
+                line = reader.readLine()
+            }
+
+            val jsonData = buffer.toString()
+
+            val obj = JSONObject(jsonData)
+            Log.e("data", "${obj}")
+
+            val bakeryData = obj.get("LOCALDATA_072218_GS") as JSONObject
+
+
+
+        } catch (e: MalformedURLException) {
+            Log.e("exception", "$e")
+        } catch (e: IOException) {
+            Log.e("exception", "$e")
+        } catch (e: JSONException) {
+            Log.e("exception", "$e")
+//        } catch (e: NetworkOnMainThreadException){
+//            Log.e("왜지..?", "왜지감자?")
+        }
     }
 
 }
