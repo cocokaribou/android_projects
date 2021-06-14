004775638304a8f7ded4e099fceb9d0d02cf567f
cocokaribou <younginlee2017@gmail.com>
2021-06-14 17:02:52 +0900

    [Update] api에서 가져온 json 작업중

---
+++ openapi_test/app/src/main/java/com/example/openapi_test/Data/DataVO.kt
@@ -0,0 +1,28 @@
+package com.example.openapi_test.Data
+
+import com.google.gson.annotations.SerializedName
+
+data class DataVO(
+    @field:SerializedName("LOCALDATA_072218_GS") var voObject: VoObject,
+) {
+
+    data class VoObject(
+        @field:SerializedName("RESULT") var result: Result,
+        @field:SerializedName("list_total_count") var listCount: Int,
+        @field:SerializedName("row") var bakeries: ArrayList<Bakery>,
+        ) {
+        data class Result(
+            @field:SerializedName("CODE") var code: String,
+            @field:SerializedName("MESSAGE") var msg: String,
+        )
+        data class Bakery(
+            @field:SerializedName("DTLSTATENM") var storeState: String,
+            @field:SerializedName("BPLCNM") var storeNm: String,
+            @field:SerializedName("SITETEL") var storeTel: String,
+            @field:SerializedName("SITEWHLADDR") var storeAdr: String,
+        )
+
+    }
+
+
+}
--- openapi_test/app/src/main/java/com/example/openapi_test/MainActivity.kt
+++ openapi_test/app/src/main/java/com/example/openapi_test/MainActivity.kt
@@ -6,19 +6,20 @@ import android.os.NetworkOnMainThreadException
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.openapi_test.Data.DataVO
+//import com.example.openapi_test.Data.Bakery
 import com.example.openapi_test.databinding.ActivityMainBinding
+import com.google.gson.Gson
 import org.json.JSONException
-import org.json.JSONObject
 import java.io.*
 import java.net.MalformedURLException
 import java.net.URL
-import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
 
     private val KEY: String = "demo" //TODO 하드코딩된 api key
     private var domain: CharSequence =
-        "http://openapi.seoul.go.kr:8088/(인증키)/json/LOCALDATA_072218_GS/1/5/"
+        "http://openapi.seoul.go.kr:8088/(인증키)/json/LOCALDATA_072218_GS/1/600/"
     private val pattern: Regex = "\\([^)]*\\)".toRegex() //괄호안 정규식
 
     lateinit var binding: ActivityMainBinding
@@ -26,7 +27,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var mainAdapter: mAdapter
     lateinit var linearManager: LinearLayoutManager
 
-    private val list = arrayListOf<String>()
+    private val bakeryList = arrayListOf<DataVO.VoObject.Bakery>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,7 +39,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setRecycler() {
-        mainAdapter = mAdapter(list)
+        mainAdapter = mAdapter(bakeryList)
         linearManager = LinearLayoutManager(this@MainActivity)
         binding.recyclerViewMainList.also {
             it.adapter = mainAdapter
@@ -47,40 +48,55 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun requestApi() {
-        domain = domain.replace(pattern, KEY)
-        try {
-            val url = URL(domain as String)
+        Thread {
+            kotlin.run {
+                domain = domain.replace(pattern, KEY)
+                try {
+                    val url = URL(domain as String)
 
-            val stream: InputStream = url.openStream()
-            val streamReader = InputStreamReader(stream)
-            val reader = BufferedReader(streamReader)
+                    val streamReader = InputStreamReader(url.openStream())
+                    val reader = BufferedReader(streamReader)
 
-            val buffer = StringBuffer()
-            var line = reader.readLine()
+                    val buffer = StringBuffer()
+                    var line = reader.readLine()
 
-            while(!line.isNullOrBlank()){
-                buffer.append(line + "\n")
-                line = reader.readLine()
+                    while (!line.isNullOrBlank()) {
+                        buffer.append(line + "\n")
+                        line = reader.readLine()
+                    }
+
+                    val jsonData = buffer.toString()
+//                    val obj = JSONObject(jsonData)
+
+                    //Gson 라이브러리로 처리
+                    val test = Gson().fromJson(jsonData, DataVO::class.java)
+
+                    test.voObject.bakeries.forEach {
+                        if(it.storeState.contains("영업")){
+                            bakeryList.add(it)
+                            Log.e("😋", "${it.storeNm}, ${it.storeState}, ${it.storeTel}, ${it.storeAdr}")
+                        }
+                    }
+
+                    //some tweaking
+//                    val list = obj.getJSONObject("LOCALDATA_072218_GS").getJSONArray("row")
+//                    val bakeryData = obj.get("LOCALDATA_072218_GS") as JSONObject
+//                    val row = bakeryData.get("row") as JSONArray
+//                    Log.e("test", "${obj.getJSONObject("LOCALDATA_072218_GS").getJSONArray("row")}")
+
+
+                } catch (e: MalformedURLException) {
+                    Log.e("exception", "$e")
+                } catch (e: IOException) {
+                    Log.e("exception", "$e")
+                } catch (e: JSONException) { //json 데이터를 잘못 다뤘을 경우
+                    Log.e("exception", "$e")
+                } catch (e: NetworkOnMainThreadException) { //thread로 감싸주지 않았을 경우
+                    Log.e("exception", "$e")
+                }
             }
+        }.start()
 
-            val jsonData = buffer.toString()
-
-            val obj = JSONObject(jsonData)
-            Log.e("data", "${obj}")
-
-            val bakeryData = obj.get("LOCALDATA_072218_GS") as JSONObject
-
-
-
-        } catch (e: MalformedURLException) {
-            Log.e("exception", "$e")
-        } catch (e: IOException) {
-            Log.e("exception", "$e")
-        } catch (e: JSONException) {
-            Log.e("exception", "$e")
-//        } catch (e: NetworkOnMainThreadException){
-//            Log.e("왜지..?", "왜지감자?")
-        }
     }
 
 }
