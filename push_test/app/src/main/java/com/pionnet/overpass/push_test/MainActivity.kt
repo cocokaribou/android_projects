a6c509acd195aabb667f83117d6669ee8e6d2821
cocokaribou <younginlee2017@gmail.com>
2021-09-13 08:52:47 +0900

    [Fix] push_test api 콜백 추가, getUnsafeOkHttpClient() 추가

--- push_test/app/src/main/java/com/pionnet/overpass/push_test/MainActivity.kt
+++ push_test/app/src/main/java/com/pionnet/overpass/push_test/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.pionnet.overpass.push_test
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.NotificationManagerCompat
 import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.installations.FirebaseInstallations
 import com.google.firebase.messaging.FirebaseMessaging
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Converter
+import retrofit2.Response
+import java.net.URLDecoder
 
 class MainActivity : AppCompatActivity() {
+    private var token: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,7 +36,7 @@ class MainActivity : AppCompatActivity() {
                             "알림을 활성화해주세요",
                             Snackbar.LENGTH_LONG
                     )
-                    .setAction("설정"){
+                    .setAction("설정") {
                         openNotificationSettings()
                     }
             snackbar.show()
@@ -36,20 +45,24 @@ class MainActivity : AppCompatActivity() {
 
         // 토큰 전송
         val firebaseMessaging = FirebaseMessaging.getInstance()
-        firebaseMessaging.token.addOnCompleteListener{
-            if(!it.isSuccessful){
+        firebaseMessaging.token.addOnCompleteListener {
+            if (!it.isSuccessful) {
                 Log.e("FCM Log", "instance 초기화 실패")
-            }else{
+                Log.e("FCM Log", "exception: ${it.exception}")
+                Log.e("FCM Log", "result: ${it.result}")
+            } else {
                 Log.e("FCM messaging token", "${it.result}")
-                val myAPI = MyAPI.getAPIService()
-                myAPI.registerToken(it.result!!)
+                Log.e("FCM messaging token", "isString = ${it.result is String}")
+                token = it.result!!
+                registerTokenAPI()
             }
         }
 
         // 구독
         firebaseMessaging.subscribeToTopic("testTopic")
     }
-    private fun openNotificationSettings(){
+
+    private fun openNotificationSettings() {
         // Links to this app's notification settings.
         val intent = Intent()
         intent.action = "android.settings.APP_NOTIFICATION_SETTINGS"
@@ -60,5 +73,29 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("android.provider.extra.APP_PACKAGE", packageName)
 
         startActivity(intent)
+    }
+
+    private fun registerTokenAPI() {
+        val myAPI = MyAPI.getAPIService()
+        val callBack: Call<ResponseBody> = myAPI.registerToken(token)
+        callBack.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                if (response.isSuccessful) {
+                    val resultBody = URLDecoder.decode(response.body()?.string(), "utf-8")
+                    Log.e("요청결과 헤더", "${response.raw().headers}")
+                    AlertDialog.Builder(this@MainActivity).setMessage("요청결과: $resultBody")
+                            .setPositiveButton("확인") { _, _ ->
+                            }.show()
+                }
+                return
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Log.e("Throwable", "$t")
+                AlertDialog.Builder(this@MainActivity).setMessage("요청결과: 실패")
+                        .setPositiveButton("확인") { _, _ ->
+                        }.show()
+            }
+        })
     }
 }
--- push_test/app/src/main/java/com/pionnet/overpass/push_test/MyAPI.kt
+++ push_test/app/src/main/java/com/pionnet/overpass/push_test/MyAPI.kt
@@ -1,20 +1,29 @@
 package com.pionnet.overpass.push_test
 
+import android.net.Uri
 import okhttp3.Dispatcher
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.ResponseBody
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 interface MyAPI {
-    @GET("token/regist")
+    @GET("/token/regist")
     fun registerToken(
-        @Query("t") token: String
+            @Query("t") token: String
     ): Call<ResponseBody>
 
     companion object {
@@ -32,11 +41,57 @@ interface MyAPI {
             }
 
             return Retrofit.Builder()
-                .baseUrl("http://10.74.105.65:22121/")
-                .client(okBuilder.build())
-                .build()
-                .create()
-//                .client(getUnsafeOkHttpClient()!!)
+                    .baseUrl("http://10.74.105.65:22121")
+                    .client(getUnsafeOkHttpClient()!!)
+                    .build()
+                    .create(MyAPI::class.java)
+        }
+        private fun getUnsafeOkHttpClient(): OkHttpClient? {
+            return try {
+                // Create a trust manager that does not validate certificate chains
+                val trustAllCerts: Array<TrustManager> = arrayOf<TrustManager>(
+                        object : X509TrustManager {
+                            @Throws(CertificateException::class)
+                            override fun checkClientTrusted(
+                                    chain: Array<X509Certificate?>?,
+                                    authType: String?
+                            ) {
+                            }
+
+                            @Throws(CertificateException::class)
+                            override fun checkServerTrusted(
+                                    chain: Array<X509Certificate?>?,
+                                    authType: String?
+                            ) {
+                                try {
+                                    chain!![0]!!.checkValidity()
+                                } catch (e: java.lang.Exception) {
+                                    throw CertificateException("Certificate not valid or trusted.")
+                                }
+                            }
+
+                            override fun getAcceptedIssuers(): Array<X509Certificate> {
+                                return arrayOf()
+                            }
+                        }
+                )
+
+                // Install the all-trusting trust manager
+                val sslContext: SSLContext = SSLContext.getInstance("SSL")
+                sslContext.init(null, trustAllCerts, SecureRandom())
+                // Create an ssl socket factory with our all-trusting manager
+                val sslSocketFactory: SSLSocketFactory = sslContext.socketFactory
+                val builder = OkHttpClient.Builder()
+
+                builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+                builder.hostnameVerifier { hostname, _ ->
+                    val baseUri = Uri.parse("http://10.74.105.65:22121")
+                    (hostname.equals(baseUri.host, ignoreCase = true))
+                }
+                builder.build()
+            } catch (e: java.lang.Exception) {
+                throw RuntimeException(e)
+            }
         }
     }
 }
