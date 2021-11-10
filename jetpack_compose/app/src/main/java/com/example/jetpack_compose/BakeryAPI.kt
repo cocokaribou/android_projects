2f716c6e8899da6fcf978e3383a28f09a272d370
cocokaribou <younginlee2017@gmail.com>
2021-11-10 16:05:08 +0900

    [Update] jetpack compose로 lazyColumn, lazyRow 구현하기
    ComposeReorderable 라이브러리 구현 작업중

---
+++ jetpack_compose/app/src/main/java/com/example/jetpack_compose/BakeryAPI.kt
@@ -0,0 +1,68 @@
+package com.example.jetpack_compose
+
+import com.example.jetpack_compose.data.Bakery
+import okhttp3.Dispatcher
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface BakeryAPI {
+
+    @GET("{key}/json/LOCALDATA_072218_GS/{page}/{total}")
+    fun getBakery(
+        @Path("key") key:String = "524d4b724e6a6f79313037777a6e6377",
+        @Path("page") page : String? = "1",
+        @Path("total") total : String? = "100"
+    ):Call<Bakery>
+
+    //retrofit annotation -> http method를 이용
+
+
+    companion object {
+        fun create(): BakeryAPI {
+            val httpLoggingInterceptor = HttpLoggingInterceptor()
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            val headerInterceptor = Interceptor {
+                val request = it.request()
+                    .newBuilder()
+                    .build()
+                return@Interceptor it.proceed((request))
+            }
+
+            val test = Dispatcher()
+
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(headerInterceptor)
+                .addInterceptor(httpLoggingInterceptor)
+                .dispatcher(test)
+                .build()
+            //OkHttp: Interceptor 지원
+            // 1) Application Interceptor: OkHttpClient.Builder().addInterceptor
+            // 2) Network Interceptor: OkHttpClient.Builder().addNetworkInterceptor
+
+
+
+            //TODO 오브젝트 구조 파싱해서 바꿔야함
+
+            return Retrofit.Builder()
+                .baseUrl("http://openapi.seoul.go.kr:8088/")
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(BakeryAPI::class.java)
+
+
+
+            //Retrofit: OkHttp를 네트워킹 계층으로 활용하며 그 위에 구축된다.
+            //JSON 응답을 사전에 정의된 POJO를 통해 직렬화 할 수 있다.
+
+        }
+    }
+}
--- jetpack_compose/app/src/main/java/com/example/jetpack_compose/MainActivity2.kt
+++ jetpack_compose/app/src/main/java/com/example/jetpack_compose/MainActivity2.kt
@@ -2,36 +2,44 @@ package com.example.jetpack_compose
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.ExperimentalUnitApi
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.*
+import com.example.jetpack_compose.data.Bakery
 import com.example.jetpack_compose.ui.theme.Jetpack_composeTheme
+import org.burnoutcrew.reorderable.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity2 : ComponentActivity() {
+    val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
     @ExperimentalUnitApi
     override fun onCreate(savedInstanceState: Bundle?) {
-        val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
         super.onCreate(savedInstanceState)
         Intent()
+
         setContent {
             Jetpack_composeTheme {
                 // A surface container using the 'background' color from the theme
@@ -42,21 +50,30 @@ class MainActivity2 : ComponentActivity() {
                             InitLazyRow(list)
                             InitLazyColumn(list)
                         }
-                        Text(
-                            text = "test tes",
-                            fontSize = TextUnit(100f, TextUnitType.Sp),
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .fillMaxHeight(),
-                            textAlign = TextAlign.Right,
-                        )
-
                     }
                 }
             }
         }
     }
+
+    private fun requestBakery(): MutableList<String> {
+        val list = mutableListOf<String>()
+        val bakeryApi = BakeryAPI.create()
+        bakeryApi.getBakery().enqueue(object : Callback<Bakery> {
+            override fun onFailure(call: Call<Bakery>, t: Throwable) {
+                Log.e("youngin", "onFailure")
+            }
+
+            override fun onResponse(call: Call<Bakery>, response: Response<Bakery>) {
+                response.body()!!.voObject.bakeries.forEach {
+                    list.add(it.storeNm)
+                }
+            }
+        })
+        return list
+    }
 }
+
 
 @Composable
 fun Title() {
@@ -72,45 +89,88 @@ fun Title() {
     }
 }
 
+@ExperimentalUnitApi
 @Composable
-fun InitLazyRow(intList: List<Int>) {
-    LazyRow(
-        horizontalArrangement = Arrangement.spacedBy(Dp(3f))
-    ) {
-        items(intList) { i ->
-            itemCompo(i)
+fun InitLazyRow(list: List<Int>) {
+    val state = rememberReorderState()
+    val data = list.toMutableStateList()
+    Box {
+        LazyRow(
+            state = state.listState,
+            horizontalArrangement = Arrangement.spacedBy(Dp(3f)),
+            modifier = Modifier
+                .reorderable(state, { from, to -> data.move(from.index, to.index) })
+        ) {
+            items(data, {it}) { i ->
+                Box(
+                    modifier = Modifier
+                        .padding(start = Dp(15f), top = Dp(15f))
+                        .draggedItem(state.offsetByKey(i))
+                        .detectReorderAfterLongPress(state),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "icon"
+                    )
+                    Text(
+                        text = i.toString(),
+                    )
+                }
+            }
+        }
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "LazyRow", color = Color(0xF000000), fontSize = TextUnit(80f, TextUnitType.Sp))
         }
     }
 }
 
+@ExperimentalUnitApi
 @Composable
-fun InitLazyColumn(intList: List<Int>) {
-    val state = rememberLazyListState()
-    LazyColumn(
-        modifier = Modifier.padding(top = Dp(20f)),
-        verticalArrangement = Arrangement.spacedBy(Dp(3f))
-    ) {
-        items(intList) { i ->
-            itemCompo(i)
+fun InitLazyColumn(list: List<Int>) {
+    val state = rememberReorderState()
+    val data = list.toMutableStateList()
+
+    Box(modifier = Modifier.padding(top = 20.dp)) {
+        // lazycolumn (recycler)
+        LazyColumn(
+            state = state.listState,
+            modifier = Modifier
+                .reorderable(state, { from, to -> data.move(from.index, to.index) }),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(list, {it}) { i ->
+                Card(shape = RoundedCornerShape(10.dp), modifier = Modifier.padding(5.dp), elevation = 3.dp) {
+                    Row(
+                        Modifier
+                            .background(Color(0xFFEFEFE8))
+                            .padding(20.dp)
+                            .fillMaxWidth()
+                            .draggedItem(state.offsetByKey(i))
+                            .detectReorderAfterLongPress(state)
+                    ) {
+                        Text("$i ")
+                        Text("item")
+                    }
+                }
+            }
+        }
+        // alpha text
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.End
+        ) {
+            Text(
+                text = "LazyColumn",
+                color = Color(0xF000000),
+                fontSize = TextUnit(50f, TextUnitType.Sp),
+                modifier = Modifier.width(35.dp),
+            )
         }
     }
 }
-
-@Composable
-fun itemCompo(i: Int) {
-    Box(
-        modifier = Modifier.padding(start = Dp(15f), top = Dp(15f)),
-        contentAlignment = Alignment.Center,
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "icon"
-        )
-        Text(
-            text = i.toString(),
-        )
-    }
-}
-
 
 @Preview(showBackground = true)
 @Composable
