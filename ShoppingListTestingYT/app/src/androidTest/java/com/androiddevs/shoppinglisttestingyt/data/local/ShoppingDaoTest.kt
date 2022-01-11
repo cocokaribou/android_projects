38b28ec94976194072e95d5acade937cdd64832c
cocokaribou <younginlee2017@gmail.com>
2022-01-11 20:39:48 +0900

    [Update] RoomDB test code

--- ShoppingListTestingYT/app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/data/local/ShoppingDaoTest.kt
+++ ShoppingListTestingYT/app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/data/local/ShoppingDaoTest.kt
@@ -1,16 +1,28 @@
 package com.androiddevs.shoppinglisttestingyt.data.local
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import com.androiddevs.shoppinglisttestingyt.getOrAwaitValue
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
+@ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 @SmallTest
 class ShoppingDaoTest {
+
+    // job is not completed
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     private lateinit var database: ShoppingItemDatabase
     private lateinit var dao: ShoppingDao
@@ -27,6 +39,50 @@ class ShoppingDaoTest {
     @After
     fun teardown() {
         database.close()
+    }
+
+    // testing suspend function, should be executed within coroutine
+    // don't want concurrency
+    // runBlocking : execute coroutine in Main thread
+    // runBlockingTest : optimized for testing
+    @Test
+    fun insertShoppingItem() = runBlockingTest{
+        val shoppingItem = ShoppingItem("name", 1, 1f, "url", id = 1)
+        dao.insertShoppingItem(shoppingItem)
+
+        // returns live data, which runs asynchronous
+        // LiveDataUtilAndroidTest.kt -> waits for the value of a livedata to be returned
+        val allShoppingItems = dao.observeAllShoppingItem().getOrAwaitValue()
+
+        assertThat(allShoppingItems).contains(shoppingItem)
+    }
+
+    @Test
+    fun deleteShoppingItem() = runBlockingTest {
+        val shoppingItem = ShoppingItem("name", 1, 1f, "url", id = 1)
+        dao.deleteShoppingItem(shoppingItem)
+
+        val allShoppingItems = dao.observeAllShoppingItem().getOrAwaitValue()
+
+        assertThat(allShoppingItems).doesNotContain(shoppingItem)
+    }
+
+    @Test
+    fun observeTotalPriceSum() = runBlockingTest {
+        val shoppingItem1 = ShoppingItem("name", 1, 10f, "url", id = 1)
+        val shoppingItem2 = ShoppingItem("name", 3, 5.5f, "url", id = 2)
+        val shoppingItem3 = ShoppingItem("name", 0, 100f, "url", id = 3)
+
+        dao.insertShoppingItem(shoppingItem1)
+        dao.insertShoppingItem(shoppingItem2)
+        dao.insertShoppingItem(shoppingItem3)
+
+        val totalPriceSum = dao.observeTotalPrice().getOrAwaitValue()
+
+        assertThat(totalPriceSum).isEqualTo(1 * 10f + 3 * 5.5f)
+
+
+
     }
 
 }
