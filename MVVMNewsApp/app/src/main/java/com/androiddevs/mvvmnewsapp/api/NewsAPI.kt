5d47074adb5b1ca85a1a2e188d52a1edb46b674b
cocokaribou <younginlee2017@gmail.com>
2022-03-20 20:43:09 +0900

    [Add] MVVMNewsApp demo

---
+++ MVVMNewsApp/app/src/main/java/com/androiddevs/mvvmnewsapp/api/NewsAPI.kt
@@ -0,0 +1,48 @@
+package com.androiddevs.mvvmnewsapp.api
+
+import com.androiddevs.mvvmnewsapp.models.NewsResponse
+import com.androiddevs.mvvmnewsapp.util.Constants.API_KEY
+import com.androiddevs.mvvmnewsapp.util.Constants.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+
+    // asynchronously
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country") countryCode: String = "us",
+        @Query("page") pageNumber: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q") searchQuery: String,
+        @Query("page") pageNumber: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    companion object {
+        private val retrofit by lazy {
+            // only initialize object once
+
+            val logging = HttpLoggingInterceptor()
+            logging.level = HttpLoggingInterceptor.Level.BODY
+            val client = OkHttpClient.Builder().addInterceptor(logging).build()
+
+            Retrofit.Builder().baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+        }
+        val api by lazy {
+            retrofit.create(NewsAPI::class.java)
+        }
+    }
+}
---
+++ MVVMNewsApp/app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.androiddevs.mvvmnewsapp.util
+
+object Constants {
+    const val API_KEY = "98eb636dda6341ffbf1995b4dbcce5e3"
+
+    const val BASE_URL = "https://newsapi.org"
+}
