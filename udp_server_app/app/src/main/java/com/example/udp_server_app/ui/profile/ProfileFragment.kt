246c51cfb6b87e6fe9a66d89ac95cdbb8b549573
cocokaribou <younginlee2017@gmail.com>
2022-03-27 16:33:35 +0900

    [Update] udp_server_app profile input process

--- udp_server_app/app/src/main/java/com/example/udp_server_app/ui/profile/ProfileFragment.kt
+++ udp_server_app/app/src/main/java/com/example/udp_server_app/ui/profile/ProfileFragment.kt
@@ -2,10 +2,13 @@ package com.example.udp_server_app.ui.profile
 
 import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
@@ -17,46 +20,62 @@ import com.example.udp_server_app.ui.profile.ProfileFragment.Companion.numArray
 import com.example.udp_server_app.R
 import com.example.udp_server_app.ui.MainActivity
 
-class ProfileFragment : Fragment(R.layout.fragment_profile) {
-    lateinit var backPressedCallback: OnBackPressedCallback
-    lateinit var pageChangeCallback: ViewPager2.OnPageChangeCallback
 
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
     private var vp1: ViewPager2? = null
     private var vp2: ViewPager2? = null
     private var vp3: ViewPager2? = null
-
     private var btnSave: Button? = null
+    private var etName: EditText? = null
 
     companion object {
         val numArray = arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
     }
 
+    // back pressed callback
+    private val backPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            (activity as MainActivity).removeFrag(this@ProfileFragment)
+        }
+    }
+
+    // viewpager listener
+    private val pageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageScrolled(
+            position: Int,
+            positionOffset: Float,
+            positionOffsetPixels: Int
+        ) {
+            super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+            Logger("scrolled to $position")
+        }
+
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+            Logger("$position selected")
+        }
+    }
+
+    // editText key listener
+    private val editTextWatcher = object:View.OnKeyListener {
+        override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                val inputMethodManager: InputMethodManager? = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+                inputMethodManager?.hideSoftInputFromWindow(view!!.windowToken, 0)
+
+                etName?.isFocusable = false
+                return true
+            }
+            return false
+        }
+
+    }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // back pressed
-        backPressedCallback = object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                (activity as MainActivity).removeFrag(this@ProfileFragment)
-            }
-        }
         activity?.onBackPressedDispatcher?.addCallback(backPressedCallback)
-
-        pageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageScrolled(
-                position: Int,
-                positionOffset: Float,
-                positionOffsetPixels: Int
-            ) {
-                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
-                Logger("scrolled to $position")
-            }
-
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                Logger("$position selected")
-            }
-        }
 
         initUI()
     }
@@ -67,6 +86,7 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
             vp2 = it.findViewById(R.id.vp_2)
             vp3 = it.findViewById(R.id.vp_3)
             btnSave = it.findViewById(R.id.btn_save_profile)
+            etName = it.findViewById(R.id.et_name)
         }
 
         vp1?.let {
@@ -82,6 +102,8 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
             it.adapter = ViewPagerAdapter(requireContext())
             it.registerOnPageChangeCallback(pageChangeCallback)
         }
+
+        etName?.setOnKeyListener(editTextWatcher)
 
         btnSave?.let {
             //TODO save view as an image
