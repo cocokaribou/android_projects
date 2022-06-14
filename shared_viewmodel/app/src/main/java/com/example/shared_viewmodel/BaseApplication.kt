674433eda391c9dc17bf4e5248d6d9c1306b7f65
cocokaribou <younginlee2017@gmail.com>
2022-06-14 16:16:42 +0900

    [Add] Flipper dependency to shared_viewmodel project

---
+++ shared_viewmodel/app/src/main/java/com/example/shared_viewmodel/BaseApplication.kt
@@ -0,0 +1,23 @@
+package com.example.shared_viewmodel
+
+import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.soloader.SoLoader
+
+class BaseApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        LogHelper.v("app started")
+
+        SoLoader.init(this, false)
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.start()
+        }
+    }
+}
