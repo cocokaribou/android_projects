bb1218668c6c8534b04b34d84931b5a33735a41e
cocokaribou <younginlee2017@gmail.com>
2023-06-07 12:49:14 +0900

    [Update] shared_viewmodel

--- shared_viewmodel/app/src/main/java/com/example/shared_viewmodel/ui/notify/NotifyFragment.kt
+++ shared_viewmodel/app/src/main/java/com/example/shared_viewmodel/ui/notify/NotifyFragment.kt
@@ -1,16 +1,22 @@
 package com.example.shared_viewmodel.ui.notify
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.shared_viewmodel.databinding.FragmentIntroBinding
 import com.example.shared_viewmodel.databinding.FragmentNotifyBinding
 
 class NotifyFragment : Fragment() {
     private val binding get() = _binding!!
     private var _binding: FragmentNotifyBinding? = null
+
+    private lateinit var callback: OnBackPressedCallback
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentNotifyBinding.inflate(inflater)
@@ -20,5 +26,16 @@ class NotifyFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().navigateUp()
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
     }
 }
